c693c6dc6afff55acaa81189500c8569f7ebac44
helgegoessling <helge.goessling@awi.de>
2018-09-18 09:08:28 +0200

    Changed paste0(...) to file.path(...) in sidfex.fcst.search.rda2csv

--- R/sidfex.fcst.search.rda2csv.R
+++ R/sidfex.fcst.search.rda2csv.R
@@ -1,9 +1,9 @@
 sidfex.fcst.search.rda2csv <- function(rTab.in, headerlines, indexTable.path=NULL){
   # create and open the file connection
-  datafile <- file(paste0(indexTable.path, "indexTable.csv"), open = 'wt')
-  # if a header is defined, write it to the file 
+  datafile <- file(file.path(indexTable.path, "indexTable.csv"), open = 'wt')
+  # if a header is defined, write it to the file
   writeLines(headerlines,con=datafile)
-  # write the file using the defined function and required addition arguments  
+  # write the file using the defined function and required additional arguments
   write.csv(rTab.in, file=datafile, row.names = FALSE)
   close(datafile)
 }
