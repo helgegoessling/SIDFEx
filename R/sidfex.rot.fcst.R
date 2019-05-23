aa8b7420c5c3658b3021dc8d28fdbd7cd0e16a64
helgegoessling <helge.goessling@awi.de>
2019-05-23 16:30:13 +0200

    Extended sidfex.rot.fcst so that the adjustments can be made also on other times than just the initial time, and so that ensembles are properly handled.

--- R/sidfex.rot.fcst.R
+++ R/sidfex.rot.fcst.R
@@ -1,43 +1,63 @@
-sidfex.rot.fcst <- function (obs=NULL,fcst,lonlat.orig=NULL,lonlat.new=NULL,rot.InitX=FALSE,data.path=NULL) {
+sidfex.rot.fcst <- function (obs=NULL,fcst,obsref.DaysLeadTime=0,obsref.Year=NULL,obsref.DayOfYear=NULL,lonlat.orig=NULL,lonlat.new=NULL,rot.InitX=TRUE,ensmean.based=FALSE,data.path=NULL) {
 
   require(spheRlab)
 
-  rl = fcst$res.list
+  # check whether fcst is a complete list of forecasts as returned from sidfex.read.fcst, or just
+  # one element from such a list; in the latter case; then, define rl from fcst consistently
+  if ("res.list" %in% names(fcst)) {
+    rl = fcst$res.list
+    single.element = FALSE
+  } else if ("data" %in% names(fcst)) {
+    rl = list(fcst)
+    single.element = TRUE
+  } else {
+    stop("format of 'fcst' not recognised.")
+  }
 
-  if (is.null(obs)) {
+  if (is.null(obs) && is.null(lonlat.orig)) {
     obs = sidfex.read.obs(TargetID = unique(sapply(fcst$res.list,"[[","TargetID")),data.path=data.path)
   }
 
   if (is.null(lonlat.orig)) {
-    initial.correction = TRUE
-    fcst.initial = sidfex.remaptime.fcst(fcst, newtime.DaysLeadTime = 0)
-    obs.initial = sidfex.remaptime.obs2fcst(obs=obs, fcst=fcst.initial)
-    rot.fcst.string = "Rotation based on initial forecast position difference from observation"
-  } else {
-    initial.correction = FALSE
-    rot.fcst.string = paste0("Rotation based on difference between pre-defined points: lonlat.orig=",
-                             lonlat.orig, ", lonlat.new=", lonlat.new)
-  }
-
-  for (irl in 1:length(rl)) {
-
-    if (initial.correction) {
-      #lonlat.orig = c(fcst$res.list[[irl]]$InitLon, fcst$res.list[[irl]]$InitLat)
-      lonlat.orig = c(fcst.initial$res.list[[irl]]$data$Lon[1], fcst.initial$res.list[[irl]]$data$Lat[1])
-      lonlat.new = c(obs.initial$res.list[[irl]]$data$Lon[1], obs.initial$res.list[[irl]]$data$Lat[1])
+    timebased = TRUE
+    if (!is.null(obsref.Year)) {
+      if (is.null(obsref.DayOfYear)) {stop("If 'obsref.Year' is specified, 'obsref.DayOfYear' must be specified as well")}
+      ydoybased = TRUE
+      fcst.ref = sidfex.remaptime.fcst(fcst, newtime.FractionOfDay = obsref.DayOfYear - floor(obsref.DayOfYear),
+                                       newtime.YearDayOfYear = data.frame(obsref.Year,floor(obsref.DayOfYear)))
+    } else {
+      ydoybased = FALSE
+      fcst.ref = sidfex.remaptime.fcst(fcst, newtime.DaysLeadTime = obsref.DaysLeadTime)
     }
-
+    obs.ref = sidfex.remaptime.obs2fcst(obs=obs, fcst=fcst.ref)
+  } else {
+    timebased = FALSE
+    # determine rotation parameters based on lonlat.orig and lonlat.new so that they lie on the equator of the rotation
     xyz.orig = sl.lonlat2xyz(lonlat.orig)
     xyz.new = sl.lonlat2xyz(lonlat.new)
     dist.angle = sl.gc.dist(lon=c(lonlat.orig[1], lonlat.new[1]), lat=c(lonlat.orig[2], lonlat.new[2]))*360/2/pi
     pole.lonlat = sl.xyz2lonlat(sl.crossvec(xyz.orig,xyz.new))
     abg.1 = sl.lonlatrot2abg(lonlatrot = c(pole.lonlat,-dist.angle))
     abg.2 = sl.lonlatrot2abg(lonlatrot = c(pole.lonlat,0))
+  }
 
-    rot.tmp = sl.rot(lon = rl[[irl]]$data$Lon, lat = rl[[irl]]$data$Lat, alpha = abg.1[1], beta = abg.1[2], gamma = abg.1[3])
-    rot = sl.rot(lon = rot.tmp$lon, lat = rot.tmp$lat, alpha = abg.2[1], beta = abg.2[2], gamma = abg.2[3], invert = TRUE)
-    rl[[irl]]$data$Lat = rot$lat
-    rl[[irl]]$data$Lon = rot$lon
+  for (irl in 1:length(rl)) {
+
+    LatCols = which(substr(names(rl[[irl]]$data), start = 1, stop = 3) == "Lat")
+    LonCols = LatCols + 1
+    Nens = length(LatCols)
+
+    if (timebased) {
+      lonlat.orig = c(fcst.ref$res.list[[irl]]$data$Lon[1], fcst.ref$res.list[[irl]]$data$Lat[1])
+      lonlat.new = c(obs.ref$res.list[[irl]]$data$Lon[1], obs.ref$res.list[[irl]]$data$Lat[1])
+      # determine rotation parameters based on lonlat.orig and lonlat.new so that they lie on the equator of the rotation
+      xyz.orig = sl.lonlat2xyz(lonlat.orig)
+      xyz.new = sl.lonlat2xyz(lonlat.new)
+      dist.angle = sl.gc.dist(lon=c(lonlat.orig[1], lonlat.new[1]), lat=c(lonlat.orig[2], lonlat.new[2]))*360/2/pi
+      pole.lonlat = sl.xyz2lonlat(sl.crossvec(xyz.orig,xyz.new))
+      abg.1 = sl.lonlatrot2abg(lonlatrot = c(pole.lonlat,-dist.angle))
+      abg.2 = sl.lonlatrot2abg(lonlatrot = c(pole.lonlat,0))
+    }
 
     if (rot.InitX) {
       init.rot.tmp = sl.rot(lon = rl[[irl]]$InitLon, lat = rl[[irl]]$InitLat, alpha = abg.1[1], beta = abg.1[2], gamma = abg.1[3])
@@ -46,8 +66,44 @@ sidfex.rot.fcst <- function (obs=NULL,fcst,lonlat.orig=NULL,lonlat.new=NULL,rot.
       rl[[irl]]$InitLon = init.rot$lon
     }
 
+    for (iens in 1:Nens) {
+
+      if (timebased && iens > 1 && !ensmean.based) {
+        lonlat.orig = c(fcst.ref$res.list[[irl]]$data[,LonCols[iens]][1], fcst.ref$res.list[[irl]]$data[,LatCols[iens]][1])
+        # determine rotation parameters based on lonlat.orig and lonlat.new so that they lie on the equator of the rotation
+        xyz.orig = sl.lonlat2xyz(lonlat.orig)
+        xyz.new = sl.lonlat2xyz(lonlat.new)
+        dist.angle = sl.gc.dist(lon=c(lonlat.orig[1], lonlat.new[1]), lat=c(lonlat.orig[2], lonlat.new[2]))*360/2/pi
+        pole.lonlat = sl.xyz2lonlat(sl.crossvec(xyz.orig,xyz.new))
+        abg.1 = sl.lonlatrot2abg(lonlatrot = c(pole.lonlat,-dist.angle))
+        abg.2 = sl.lonlatrot2abg(lonlatrot = c(pole.lonlat,0))
+      }
+
+      # do the rotation
+      rot.tmp = sl.rot(lon = rl[[irl]]$data[,LonCols[iens]], lat = rl[[irl]]$data[,LatCols[iens]], alpha = abg.1[1], beta = abg.1[2], gamma = abg.1[3])
+      rot = sl.rot(lon = rot.tmp$lon, lat = rot.tmp$lat, alpha = abg.2[1], beta = abg.2[2], gamma = abg.2[3], invert = TRUE)
+      rl[[irl]]$data[,LatCols[iens]] = rot$lat
+      rl[[irl]]$data[,LonCols[iens]] = rot$lon
+
+      if (rot.InitX && !is.null(rl[[irl]]$MergedInitLat)) {
+        init.rot.tmp = sl.rot(lon = rl[[irl]]$MergedInitLon[iens], lat = rl[[irl]]$MergedInitLat[iens], alpha = abg.1[1], beta = abg.1[2], gamma = abg.1[3])
+        init.rot = sl.rot(lon = init.rot.tmp$lon, lat = init.rot.tmp$lat, alpha = abg.2[1], beta = abg.2[2], gamma = abg.2[3], invert = TRUE)
+        rl[[irl]]$MergedInitLat[iens] = init.rot$lat
+        rl[[irl]]$MergedInitLon[iens] = init.rot$lon
+      }
+
+    }
+
   }
 
-  return(list(ens.merge=fcst$ens.merge, rot.fcst=rot.fcst.string, res.list=rl))
+  if (single.element) {
+    return(rl[[1]])
+  } else {
+    return(list(ens.merge=fcst$ens.merge, rot.fcst.arguments=list(obsref.DaysLeadTime=obsref.DaysLeadTime,obsref.Year=obsref.Year,
+                                                                obsref.DayOfYear=obsref.DayOfYear,lonlat.orig=lonlat.orig,
+                                                                lonlat.new=lonlat.new,rot.InitX=rot.InitX,
+                                                                ensmean.based=ensmean.based),
+              res.list=rl))
+  }
 
 }
