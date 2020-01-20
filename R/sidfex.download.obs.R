341bb247241e257c9a30840d98a91344cf17662d
helgegoessling <helge.goessling@awi.de>
2020-01-20 09:40:59 +0100

    increase try.timeout default to 60s

--- R/sidfex.download.obs.R
+++ R/sidfex.download.obs.R
@@ -1,4 +1,4 @@
-sidfex.download.obs <- function(index=NULL,TargetID=NULL,data.path=NULL,baseurl=NULL,try.N=30,try.timeout=20) {
+sidfex.download.obs <- function(index=NULL,TargetID=NULL,data.path=NULL,baseurl=NULL,try.N=30,try.timeout=60) {
 
   if (is.null(data.path)) {
     no.data.path.obs=TRUE
